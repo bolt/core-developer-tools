3622f5dcfa98384f9f04ca80663830df27978779
Gawain Lynch <gawain.lynch@gmail.com>
2018-05-09 12:14:51 +0200

    Extension loader

---
+++ src/ToolsExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\CoreDeveloper;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * An extension for helping core-developers.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class ToolsExtension extends SimpleExtension
+{
+}
